4e14508e3b15cb5ecd8aa8689bebf95063070908
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2022-02-02 21:35:39 +0100

    predfinalni komit, ostalo par sitnih izmena

--- lim-rafting/src/components/Cards.js
+++ lim-rafting/src/components/Cards.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import CardItem from './CardItem';
-import './Cards.css';
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
 
-const Cards = () =>
+function Cards()
 {
 	return (
 		<div className="cards">
 			<h1>Pogledaj šta te sve čeka kod nas!</h1>
-			<div className='cards__container'>
-				<div className='cards__wrapper'>
-					<ul className='cards__items'>
+			<div className="cards__container">
+				<div className="cards__wrapper">
+					<ul className="cards__items">
 						<CardItem
-							src="../components/images/rafting1.jpg"
+							src="../images/jungle.png"
 							text="Istrazi skrivene brzake Lima"
 							label="Avantura"
 							path="/"
 						/>
 						<CardItem
-							src="../components/images/rafting1.jpg"
+							src="../images/rafting1.jpg"
 							text="Osvoji Crni Vrh i doživi pravo planinarsko iskustvo"
 							label="Planinarenje"
 							path="/"
 						/>
+					</ul>
+					<ul className="cards__items">
 						<CardItem
-							src="../components/images/rafting1.jpg"
+							src="../images/rafting1.jpg"
 							text="Ako si planinar početnik, Lisja stena je prilika da se oprobaš"
 							label="Planinarenje"
 							path="/"
 						/>
 						<CardItem
-							src="../components/images/rafting1.jpg"
+							src="../images/rafting1.jpg"
 							text="Zaplovi meandrima Uvca - iskusi pravu prirodu"
+							label="Avantura"
+							path="/"
+						/>
+						<CardItem
+							src="../images/rafting1.jpg"
+							text="Oseti svežinu čistog vazduha Zlatibora"
 							label="Avantura"
 							path="/"
 						/>
@@ -39,6 +47,6 @@ const Cards = () =>
 			</div>
 		</div>
 	);
-};
+}
 
 export default Cards;
